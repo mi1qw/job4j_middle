eccf76711ecfa77fff12587a0d3e36a7ffc30b9a
mib1 <mib1@inbox.ru>
2020-08-19 09:20:57 +0300

    method kBsPause

--- chapter_001/src/main/java/ru/job4j/filedownload/FileDownload.java
+++ chapter_001/src/main/java/ru/job4j/filedownload/FileDownload.java
@@ -20,25 +20,22 @@ public class FileDownload {
 
     public static void main(final String[] args) throws Exception {
         String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        double byteSec = 200;
-        long timest = 0;
-        int totalbyte = 0;
+        int byteSec = 200;
         File pom = File.createTempFile("temp", ".xml");
         try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
              FileOutputStream fileOutputStream = new FileOutputStream(pom)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            timest = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(round((double) bytesRead / byteSec));
-                totalbyte += bytesRead;
+                Thread.sleep(kBsPause(byteSec, bytesRead));
             }
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
         }
-        timest = System.currentTimeMillis() - timest;
-        System.out.println(totalbyte + "  " + timest);
-        System.out.println((double) totalbyte / timest);
+    }
+
+    public static long kBsPause(final double byteMsec, final int bytes) {
+        return round((double) bytes / byteMsec);
     }
 }
