1fad09e8858f69eefef98519ab6d69252e4bb6b1
mib1 <mib1@inbox.ru>
2020-08-24 17:45:30 +0300

    2

--- chapter_001/src/main/java/ru/job4j/stopconsumer/ParallelSearch.java
+++ chapter_001/src/main/java/ru/job4j/stopconsumer/ParallelSearch.java
@@ -3,23 +3,23 @@ package ru.job4j.stopconsumer;
 import ru.job4j.simpleblockingqueue.SimpleBlockingQueue;
 
 public class ParallelSearch {
-    public static void main(final String[] args) {
-        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>(5);
+    public static void main(final String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(50);
         final Thread consumer = new Thread(
                 () -> {
                     while (true) {
                         try {
                             System.out.println(queue.poll());
-                            Thread.sleep(1);
+                            Thread.sleep(10);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                             Thread.currentThread().interrupt();
                         }
                     }
-                }
+                }, "Consumer"
         );
         consumer.start();
-        new Thread(
+        final Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         queue.offer(index);
@@ -27,10 +27,20 @@ public class ParallelSearch {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
+                            Thread.currentThread().interrupt();
                         }
                     }
-                }
-
-        ).start();
+                }, "Producer"
+        );
+        producer.start();
+        producer.join();
+        System.out.println(producer.isInterrupted() + "  " + producer.getState());
+        queue.offer(0);
+        consumer.interrupt();
+        System.out.println(consumer.isInterrupted() + "  " + consumer.getState());
+        consumer.di
+        //consumer.stop();
+        System.out.println("End");
+        //consumer.join();
     }
 }
