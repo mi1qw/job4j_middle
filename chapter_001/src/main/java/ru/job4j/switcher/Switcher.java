84a6648a006889eeb512cb4ba95b1f1c2a911333
mib1 <mib1@inbox.ru>
2020-10-04 06:37:25 +0300

    start A

--- chapter_001/src/main/java/ru/job4j/switcher/Switcher.java
+++ chapter_001/src/main/java/ru/job4j/switcher/Switcher.java
@@ -3,22 +3,27 @@ package ru.job4j.switcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Switcher {
     public static final Logger LOGGER = LoggerFactory.getLogger(Switcher.class);
     private static final Object LOCK = new Object();
 
     public static void main(final String[] args) throws InterruptedException {
-
+        AtomicBoolean start = new AtomicBoolean(true);
         Thread first = new Thread(
                 () -> {
+                    start.set(false);
                     while (true) {
                         synchronized (LOCK) {
-                            System.out.println("Thread A");
                             try {
-                                Thread.sleep(1000);
+                                System.out.println("Thread A");
+                                Thread.sleep(200);
+                                LOCK.notifyAll();
                                 LOCK.wait();
                             } catch (InterruptedException e) {
-                                e.printStackTrace();
+                                Thread.currentThread().interrupt();
+                                LOGGER.error(e.getMessage(), e);
                             }
                         }
                     }
@@ -26,15 +31,26 @@ public class Switcher {
         );
         Thread second = new Thread(
                 () -> {
+                    try {
+                        while (start.get()) {
+                            synchronized (LOCK) {
+                                LOCK.wait();
+                            }
+                        }
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                        LOGGER.error(e.getMessage(), e);
+                    }
                     while (true) {
                         try {
                             synchronized (LOCK) {
-                                LOCK.wait(100);
                                 System.out.println("Thread B");
-                                Thread.sleep(1000);
+                                Thread.sleep(200);
                                 LOCK.notifyAll();
+                                LOCK.wait();
                             }
                         } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
                             LOGGER.error(e.getMessage(), e);
                         }
                     }
